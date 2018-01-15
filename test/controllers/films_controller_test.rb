ae1aee1995f64f4e5302df3e06676664957e2d57
drothschild <david@rothschildsolutions.com>
2018-01-14 19:24:08 -0800

    Remove useless text

--- test/controllers/films_controller_test.rb
+++ test/controllers/films_controller_test.rb
@@ -10,30 +10,9 @@ class FilmsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should create film" do
-  #   assert_difference('Film.count') do
-  #     post films_url, params: { film: {  } }, as: :json
-  #   end
-
-  #   assert_response 201
-  # end
-
   test "should show film" do
     get api_v1_film_url(@film), as: :json
     assert_response :success
   end
-
-  # test "should update film" do
-  #   patch film_url(@film), params: { film: {  } }, as: :json
-  #   assert_response 200
-  # end
-
-  # test "should destroy film" do
-  #   assert_difference('Film.count', -1) do
-  #     delete film_url(@film), as: :json
-  #   end
-
-  #   assert_response 204
-  # end
 
 end
